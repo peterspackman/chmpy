1566cebef1e3e080351e68417f3751d9dd1847ec
Peter Spackman <peterspackman@fastmail.com>
2020-05-07 11:26:51 +0800

    Added point group data

--- shmolecule/sfac/__init__.py
+++ shmolecule/sfac/__init__.py
@@ -3,21 +3,33 @@ from . import _sfac
 import numpy as np
 
 
-UNIQUE_REFLECTION_COMBINATIONS = { ("triclinic", "-1", "*"): (
+UNIQUE_REFLECTION_MULTIPLICITY = {
+    ("triclinic", "-1"): 2,
+    ("monoclinic", "2/m"): 4,
+    ("orthorhombic", "mmm"): 8,
+    ("tetragonal", "4/m"): 8,
+    ("tetragonal", "4/mmm"): 16,
+    ("cubic", "m-3"): 24,
+    ("cubic", "m-3m"): 48,
+}
+
+
+UNIQUE_REFLECTION_TYPES = {
+    ("triclinic", "-1", "*"): (
         ((0, 0, 0), ((1, 0, 0), (0, 1, 0), (0, 0, 1))),
         ((-1, 0, 1), ((-1, 0, 0), (0, 1, 0), (0, 0, 1))),
         ((-1, 1, 0), ((-1, 0, 0), (0, 1, 0), (0, 0, -1))),
         ((0, 1, -1), ((1, 0, 0), (0, 1, 0), (0, 0, -1)))
     ),
-    ("monoclinic", "2/m", "A"): (
+    ("monoclinic", "2/m", "aface"): (
         ((0, 0, 0), ((0, 1, 0), (1, 0, 0), (0, 0, 1))),
         ((0, -1, 1), ((0, -1, 0), (1, 0, 0), (0, 0, 1))),
     ),
-    ("monoclinic", "2/m", "B"): (
+    ("monoclinic", "2/m", "bface"): (
         ((0, 0, 0), ((1, 0, 0), (0, 1, 0), (0, 0, 1))),
         ((-1, 0, 1), ((-1, 0, 0), (0, 1, 0), (0, 0, 1))),
     ),
-    ("monoclinic", "2/m", "C"): (
+    ("monoclinic", "2/m", "cface"): (
         ((0, 0, 0), ((1, 0, 0), (0, 0, 1), (0, 1, 0))),
         ((-1, 1, 0), ((-1, 0, 0), (0, 0, 1), (0, 1, 0)))
     ),
@@ -65,21 +77,85 @@ UNIQUE_REFLECTION_COMBINATIONS = { ("triclinic", "-1", "*"): (
     )
 }
 
-def pruned_reflections(crystal, wavelength):
-    a = crystal.unit_cell.a
-    b = crystal.unit_cell.b
-    c = crystal.unit_cell.c
+def reflections(crystal, wavelength=0.7, size=10):
+    h_max, k_max, l_max = size, size, size
     recip = crystal.unit_cell.reciprocal_lattice.copy()
-    apexes, mats = [], []
-    for apex, mat in UNIQUE_REFLECTION_COMBINATIONS[("orthorhombic", "mmm", "*")]:
-        apexes.append(np.array(apex, dtype=np.int32))
-        mats.append(np.array(mat, dtype=np.int32))
-    apexes = np.vstack(apexes)
-    mats = np.stack(mats)
-    return _sfac.pruned_reflections(a, b, c, recip, apexes, mats, wavelength)
+    system = crystal.space_group.crystal_system
+    centering = "*" if system != "monoclinic" else crystal.space_group.centering
+    laue_class = crystal.space_group.laue_class
+ 
+    apexes, bases = zip(*UNIQUE_REFLECTION_TYPES[(system, laue_class, centering)])
+    h_range = np.r_[0:h_max + 1]
+    k_range = np.r_[0:k_max + 1]
+    l_range = np.r_[0:l_max + 1]
+    sections = []
+    for a, b in zip(apexes, bases):
+        v1 = np.r_[b[0][0], b[0][1], b[0][2]]
+        v2 = np.r_[b[1][0], b[1][1], b[1][2]]
+        v3 = np.r_[b[2][0], b[2][1], b[2][2]]
+        hkl = v1 * h_range[:, None]
+        hkl = hkl + (v2 * k_range[:, None])[:, None]
+        hkl = hkl + (v3 * l_range[:, None, None])[:, None, None]
+        hkl = hkl.reshape(-1, 3)
+        hkl += a
+        sections.append(hkl)
+    hkl = np.vstack(sections)
+    q = np.dot(hkl, recip)
+    q_mag = np.linalg.norm(q, axis=1)
+    mask = q_mag < (1/wavelength)
+    return q[mask], q_mag[mask], hkl[mask]
 
-def calculate_structure_factors(crystal):
-    indices = [
+
+def powder_pattern(crystal, wavelength=0.7, two_theta_range=(5, 50)):
+    hkl, q, q_mag, sfac = calculate_structure_factors(crystal, wavelength=wavelength)
+    f2 = np.abs(sfac * sfac)
+    theta = np.arcsin(wavelength * q_mag / (4 * np.pi))
+    two_theta = 2 * theta
+    l, u = np.radians(two_theta_range)
+    l = max(l, 1e-3)
+    mask = (two_theta <= u) & (two_theta >= l)
+    t = theta[mask]
+    tt = two_theta[mask]
+    i = f2[mask]
+    costt = np.cos(tt)
+    sintt = np.sin(tt)
+    scaled_intensities = i * (1 + costt * costt) / (8 * sintt * sintt * np.cos(t))
+    return np.degrees(tt), scaled_intensities
+
+
+def calculate_structure_factors(crystal, wavelength=0.7):
+    q, q_mag, hkl = reflections(crystal, wavelength=wavelength)
+    uc = crystal.unit_cell_atoms()
+    asym_sfac_idx = [
         _sfac.get_form_factor_index(el.symbol)
-        for el in crystal.elements
+        for el in crystal.asymmetric_unit.elements
     ]
+    indices = [asym_sfac_idx[i] for i in uc["asym_atom"]]
+    N = len(indices)
+    unique_indices = set(asym_sfac_idx)
+    q2 = q_mag * q_mag
+    sintl = q2 / (16 * np.pi * np.pi)
+    scattering_factors = {
+        i: _sfac.scattering_factors(i, sintl)
+        for i in unique_indices
+    }
+    frac_pos = uc["frac_pos"]
+    exp_fac = np.zeros(hkl.shape[0], dtype=np.complex128)
+    exp_fac.imag = 2 * np.pi
+    sfac = np.zeros_like(exp_fac)
+    for i, idx in enumerate(indices):
+        fj = scattering_factors[idx]
+        pos = frac_pos[i]
+        sfac += fj * np.exp(exp_fac * np.dot(hkl, pos))
+    return hkl, q, q_mag, sfac
+
+#   for (size_t atom = 0; atom < uc_elements.size(); atom++) {
+#       const auto el = uc_elements[atom];
+#       const auto& fj = fjs[el];
+#       const auto& pos = xt::view(uc_positions, atom, xt::all());
+#       auto fj_dw = fj * xt::exp(-U[atom] * q2);
+#       F_calc.add_contribution_from(pos, fj_dw);
+#        np.exp
+        
+#        -2i * M_PI * xt::linalg::dot(m_hkl, pos_frac);
+
--- shmolecule/sfac/_sfac.pyx
+++ shmolecule/sfac/_sfac.pyx
@@ -7,8 +7,7 @@ from os.path import join, dirname
 # Enable low level memory management
 from libcpp.vector cimport vector
 from libc.stdlib cimport malloc, free
-from libc.math cimport exp, ceil, sqrt
-
+from libc.math cimport exp, ceil, sqrt 
 _DATA_DIR = dirname(__file__)
 _FORM_FACTOR_DATA = np.load(join(_DATA_DIR, "atomic_form_factors.npz"))
 _FORM_FACTOR_KEYS = _FORM_FACTOR_DATA.f.keys
@@ -19,39 +18,11 @@ _FORM_FACTOR_VALUES = _FORM_FACTOR_DATA.f.values
 def get_form_factor_index(key):
     return _FORM_FACTOR_KEYS_LIST.index(key)
 
-
-cdef class MillerIndex:
-    cdef int h, k, l
-    cdef double mag
-    def __init__(self, h, k, l):
-        self.h = h
-        self.k = k
-        self.l = l
-        self.mag = -1.0
-
-    cdef bint is_zero(self) nogil:
-        return self.h == 0 and self.k == 0 and self.l == 0
-
-    cdef void plus(self, int h, int k, int l) nogil:
-        self.h = self.h + h
-        self.k = self.k + k
-        self.l = self.l + l
-
-    cdef void dot(self, int[:, ::1] mat) nogil:
-        cdef int h = self.h
-        cdef int k = self.k
-        cdef int l = self.l
-        self.h = mat[0, 0] * h + mat[0, 1] * k + mat[0, 1] * l
-        self.k = mat[1, 0] * h + mat[1, 1] * k + mat[1, 1] * l
-        self.l = mat[2, 0] * h + mat[2, 1] * k + mat[2, 1] * l
-
-    cdef void magnitude(self, double[:, ::1] recip) nogil:
-        cdef double q1, q2, q3
-        q1 = recip[0, 0] * self.h + recip[0, 1] * self.k + recip[0, 2] * self.l
-        q2 = recip[1, 0] * self.h + recip[1, 1] * self.k + recip[1, 2] * self.l
-        q2 = recip[2, 0] * self.h + recip[2, 1] * self.k + recip[2, 2] * self.l
-        self.mag = sqrt(q1 * q1 + q2 * q2 + q3 * q3)
-
+cpdef scattering_factors(idx, sintl):
+    cdef Aff form_factor = Aff(idx)
+    result = np.empty(sintl.shape, np.float64)
+    form_factor.calc(sintl, result)
+    return result
 
 cdef class Aff:
     cdef double a1
@@ -75,9 +46,10 @@ cdef class Aff:
         self.b4 = -v[7]
         self.c = v[8]
 
-    cdef void calc(self, const double[::1] s, double[::1] fac):
+    cdef void calc(self, const double[::1] s, double[::1] fac) nogil:
         cdef int i = 0
-        for i in s.shape[0]:
+        cdef int N = s.shape[0]
+        for i in range(N):
             fac[i] = (
                 self.a1 * exp(self.b1 * s[i]) +
                 self.a2 * exp(self.b2 * s[i]) +
@@ -85,60 +57,3 @@ cdef class Aff:
                 self.a4 * exp(self.b4 * s[i]) + self.c
             )
 
-
-cdef void pruned_reflections_c(
-        double a, double b, double c, double[:,::1] recip,
-        int[:,::1] apexes, int[:,:, ::1] mats, double wavelength,
-        vector[int]* hs, vector[int]* ks, vector[int]* ls,
-        vector[double]* mags):
-    cdef int h_max = int(ceil(a * wavelength))
-    cdef int k_max = int(ceil(b * wavelength))
-    cdef int l_max = int(ceil(c * wavelength))
-    cdef int i, h1, k1, l1
-    cdef int ah, ak, al
-    cdef int[:, ::1] mat
-    cdef MillerIndex m = MillerIndex(0, 0, 0)
-    cdef double threshold
-
-    with nogil:
-        threshold = 1 / wavelength
-        for i in range(apexes.shape[0]):
-            ah = apexes[i, 0]
-            ak = apexes[i, 1]
-            al = apexes[i, 2]
-            mat = mats[i, :, :]
-            for h1 in range(0, h_max + 1):
-                for k1 in range(0, k_max + 1):
-                    for l1 in range(0, l_max + 1):
-                        m.h = h1
-                        m.k = k1
-                        m.l = l1
-                        m.mag = -1.0
-                        m.dot(mat)
-                        m.plus(ah, ak, al)
-                        if m.is_zero():
-                            continue
-                        #if (m.is_centering_absent(centering)) continue;
-                        m.magnitude(recip)
-                        if (m.mag > threshold):
-                            break
-                        hs.push_back(m.h)
-                        ks.push_back(m.k)
-                        ls.push_back(m.l)
-                        mags.push_back(m.mag)
-
-cpdef pruned_reflections(a, b, c, recip, apexes, mats, wavelength):
-    # minimum d spacing set to radius of ewald sphere
-    cdef vector[int] hs
-    cdef vector[int] ks
-    cdef vector[int] ls
-    cdef vector[double] mags
-    cdef int i
-
-    pruned_reflections_c(a, b, c, recip, apexes, mats, wavelength, &hs, &ks, &ls, &mags)
-    idxs = []
-    magnitudes = []
-    for i in range(mags.size()):
-        idxs.append((hs[i], ks[i], ls[i]))
-        magnitudes.append(mags[i])
-    return np.vstack(idxs), np.vstack(mags)
