Fix theta calculation for powder pattern

shmolecule/molecule.py
@@ -690,7 +690,9 @@ class Molecule:
         return cls([Element[x] for x in elements], np.array(positions), **kwargs)
 
     def mask(self, mask, **kwargs):
-        return Molecule.from_arrays(self.atomic_numbers[mask], self.positions[mask], **kwargs)
+        return Molecule.from_arrays(
+            self.atomic_numbers[mask], self.positions[mask], **kwargs
+        )
 
     @classmethod
     def from_sdf_dict(cls, sdf_dict, **kwargs):
@@ -701,7 +703,7 @@ class Molecule:
         m = cls(elements, positions, **sdf_dict["data"])
         if "sdf" in sdf_dict:
             m.properties["sdf"] = sdf_dict["sdf"]
-        return  m
+        return m
 
     @classmethod
     def from_sdf_file(cls, filename, **kwargs):

shmolecule/sfac/__init__.py
@@ -1,7 +1,12 @@
 from . import atomic_form_factors
 from . import _sfac
 import numpy as np
+from collections import namedtuple
 
+
+StructureFactors = namedtuple("StructureFactors", "hkl q q_mag values normalization")
+
+LAMBDA_Cu = 1.54056
 
 UNIQUE_REFLECTION_MULTIPLICITY = {
     ("triclinic", "-1"): 2,
@@ -19,7 +24,7 @@ UNIQUE_REFLECTION_TYPES = {
         ((0, 0, 0), ((1, 0, 0), (0, 1, 0), (0, 0, 1))),
         ((-1, 0, 1), ((-1, 0, 0), (0, 1, 0), (0, 0, 1))),
         ((-1, 1, 0), ((-1, 0, 0), (0, 1, 0), (0, 0, -1))),
-        ((0, 1, -1), ((1, 0, 0), (0, 1, 0), (0, 0, -1)))
+        ((0, 1, -1), ((1, 0, 0), (0, 1, 0), (0, 0, -1))),
     ),
     ("monoclinic", "2/m", "aface"): (
         ((0, 0, 0), ((0, 1, 0), (1, 0, 0), (0, 0, 1))),
@@ -31,28 +36,22 @@ UNIQUE_REFLECTION_TYPES = {
     ),
     ("monoclinic", "2/m", "cface"): (
         ((0, 0, 0), ((1, 0, 0), (0, 0, 1), (0, 1, 0))),
-        ((-1, 1, 0), ((-1, 0, 0), (0, 0, 1), (0, 1, 0)))
+        ((-1, 1, 0), ((-1, 0, 0), (0, 0, 1), (0, 1, 0))),
     ),
-    ("orthorhombic", "mmm", "*"): (
-        ((0, 0, 0), ((1, 0, 0), (0, 1, 0), (0, 0, 1))),
-    ),
-    ("tetragonal", "4/mmm", "*"): (
-        ((0, 0, 0), ((1, 0, 0), (1, 1, 0), (0, 0, 1))),
-    ),
+    ("orthorhombic", "mmm", "*"): (((0, 0, 0), ((1, 0, 0), (0, 1, 0), (0, 0, 1))),),
+    ("tetragonal", "4/mmm", "*"): (((0, 0, 0), ((1, 0, 0), (1, 1, 0), (0, 0, 1))),),
     ("tetragonal", "4/m", "*"): (
         ((0, 0, 0), ((1, 0, 0), (1, 1, 0), (0, 0, 1))),
         ((1, 2, 0), ((1, 1, 0), (0, 1, 0), (0, 0, 1))),
     ),
-    ("hexagonal", "6/mmm", "*"): (
-        ((0, 0, 0), ((1, 0, 0), (1, 1, 0), (0, 0, 1))),
-    ),
+    ("hexagonal", "6/mmm", "*"): (((0, 0, 0), ((1, 0, 0), (1, 1, 0), (0, 0, 1))),),
     ("hexagonal", "6/m", "*"): (
         ((0, 0, 0), ((1, 0, 0), (1, 1, 0), (0, 0, 1))),
         ((1, 2, 0), ((0, 1, 0), (1, 1, 0), (0, 0, 1))),
     ),
     ("hexagonal", "-3m1", "*"): (
         ((0, 0, 0), ((1, 0, 0), (1, 1, 0), (0, 0, 1))),
-        ((0, 1, 1), ((0, 1, 0), (1, 1, 0), (0, 0, 1)))
+        ((0, 1, 1), ((0, 1, 0), (1, 1, 0), (0, 0, 1))),
     ),
     ("hexagonal", "-31m", "*"): (
         ((0, 0, 0), ((1, 0, 0), (1, 1, 0), (0, 0, 1))),
@@ -60,34 +59,36 @@ UNIQUE_REFLECTION_TYPES = {
     ),
     ("hexagonal", "-3m", "*"): (
         ((0, 0, 0), ((1, 0, 0), (1, 0, -1), (1, 1, 1))),
-        ((1, 1, 0), ((1, 0, -1), (0, 0, -1), (1, 1, 1)))
+        ((1, 1, 0), ((1, 0, -1), (0, 0, -1), (1, 1, 1))),
     ),
     ("hexagonal", "-3", "*"): (
         ((0, 0, 0), ((1, 0, 0), (1, 0, -1), (1, 1, 1))),
         ((1, 1, 0), ((1, 0, -1), (0, 0, -1), (1, 1, 1))),
-        ((0, -1, -2), ((1, 0, 0), (1, 0,-1), (-1, -1, -1))),
-        ((1, 0, -2),  ((1, 0, -1), (0, 0,-1), (-1, -1, -1))),
+        ((0, -1, -2), ((1, 0, 0), (1, 0, -1), (-1, -1, -1))),
+        ((1, 0, -2), ((1, 0, -1), (0, 0, -1), (-1, -1, -1))),
     ),
-    ("cubic", "m3m", "*"): (
-        ((0, 0, 0), ((1, 0, 0), (1, 1, 0), (1, 1, 1))),
-    ),
+    ("cubic", "m3m", "*"): (((0, 0, 0), ((1, 0, 0), (1, 1, 0), (1, 1, 1))),),
     ("cubic", "m3", "*"): (
         ((0, 0, 0), ((1, 0, 0), (1, 1, 0), (1, 1, 1))),
-        ((1, 2, 0), ((0, 1, 0), (1, 1, 0), (1, 1, 1)))
-    )
+        ((1, 2, 0), ((0, 1, 0), (1, 1, 0), (1, 1, 1))),
+    ),
 }
 
-def reflections(crystal, wavelength=0.7, size=10):
+
+def reflections(crystal, wavelength=LAMBDA_Cu, size=10):
+    """Calculate the unique reflections contributing to 
+    diffraction for the given crystal, along with the q
+    vector at the provided wavelength"""
     h_max, k_max, l_max = size, size, size
     recip = crystal.unit_cell.reciprocal_lattice.copy()
     system = crystal.space_group.crystal_system
     centering = "*" if system != "monoclinic" else crystal.space_group.centering
     laue_class = crystal.space_group.laue_class
- 
+
     apexes, bases = zip(*UNIQUE_REFLECTION_TYPES[(system, laue_class, centering)])
-    h_range = np.r_[0:h_max + 1]
-    k_range = np.r_[0:k_max + 1]
-    l_range = np.r_[0:l_max + 1]
+    h_range = np.r_[0 : h_max + 1]
+    k_range = np.r_[0 : k_max + 1]
+    l_range = np.r_[0 : l_max + 1]
     sections = []
     for a, b in zip(apexes, bases):
         v1 = np.r_[b[0][0], b[0][1], b[0][2]]
@@ -102,14 +103,17 @@ def reflections(crystal, wavelength=0.7, size=10):
     hkl = np.vstack(sections)
     q = np.dot(hkl, recip)
     q_mag = np.linalg.norm(q, axis=1)
-    mask = q_mag < (1/wavelength)
+    mask = q_mag < (1 / wavelength)
     return q[mask], q_mag[mask], hkl[mask]
 
 
-def powder_pattern(crystal, wavelength=0.7, two_theta_range=(5, 50)):
-    hkl, q, q_mag, sfac = calculate_structure_factors(crystal, wavelength=wavelength)
-    f2 = np.abs(sfac * sfac)
-    theta = np.arcsin(wavelength * q_mag / (4 * np.pi))
+def powder_pattern(crystal, wavelength=LAMBDA_Cu, two_theta_range=(5, 50)):
+    """Calculate the powder pattern the given crystal at a given wavelength
+    over a given range of 2-theta"""
+    sfac = structure_factors(crystal, wavelength=wavelength)
+    hkl, q, q_mag, sfac, norm = sfac
+    f2 = np.abs(sfac * sfac) / norm
+    theta = np.arcsin(wavelength * q_mag / 2)
     two_theta = 2 * theta
     l, u = np.radians(two_theta_range)
     l = max(l, 1e-3)
@@ -123,7 +127,9 @@ def powder_pattern(crystal, wavelength=0.7, two_theta_range=(5, 50)):
     return np.degrees(tt), scaled_intensities
 
 
-def calculate_structure_factors(crystal, wavelength=0.7):
+def structure_factors(crystal, wavelength=LAMBDA_Cu):
+    """Calculate the structure factors for a given crystal at the provided
+    wavelength"""
     q, q_mag, hkl = reflections(crystal, wavelength=wavelength)
     uc = crystal.unit_cell_atoms()
     asym_sfac_idx = [
@@ -135,27 +141,15 @@ def calculate_structure_factors(crystal, wavelength=0.7):
     unique_indices = set(asym_sfac_idx)
     q2 = q_mag * q_mag
     sintl = q2 / (16 * np.pi * np.pi)
-    scattering_factors = {
-        i: _sfac.scattering_factors(i, sintl)
-        for i in unique_indices
-    }
+    scattering_factors = {i: _sfac.scattering_factors(i, sintl) for i in unique_indices}
     frac_pos = uc["frac_pos"]
     exp_fac = np.zeros(hkl.shape[0], dtype=np.complex128)
     exp_fac.imag = 2 * np.pi
     sfac = np.zeros_like(exp_fac)
+    normalization = np.zeros_like(q_mag)
     for i, idx in enumerate(indices):
         fj = scattering_factors[idx]
         pos = frac_pos[i]
         sfac += fj * np.exp(exp_fac * np.dot(hkl, pos))
-    return hkl, q, q_mag, sfac
-
-#   for (size_t atom = 0; atom < uc_elements.size(); atom++) {
-#       const auto el = uc_elements[atom];
-#       const auto& fj = fjs[el];
-#       const auto& pos = xt::view(uc_positions, atom, xt::all());
-#       auto fj_dw = fj * xt::exp(-U[atom] * q2);
-#       F_calc.add_contribution_from(pos, fj_dw);
-#        np.exp
-        
-#        -2i * M_PI * xt::linalg::dot(m_hkl, pos_frac);
-
+        normalization += fj * fj
+    return StructureFactors(hkl, q, q_mag, sfac, normalization)