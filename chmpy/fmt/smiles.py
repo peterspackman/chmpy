007e49cc99a001dc9507c13f3c63c92a79b68e42
Peter Spackman <peterspackman@fastmail.com>
2020-05-21 09:22:28 +0800

    Updated smiles parser

--- chmpy/fmt/smiles.py
+++ chmpy/fmt/smiles.py
@@ -2,60 +2,106 @@ import pyparsing as pp
 from pyparsing import Literal as Lit
 from pyparsing import Optional as Opt
 from pyparsing import Regex, oneOf
-from chmpy.core.element import element_symbols
+from chmpy.core.element import Element
 import numpy as np
 
-OrganicSymbol = Regex("Br?|Cl?|N|O|P|S|F|I|At|Ts|b|c|n|o|p|s")
-Symbol = Regex(
-    "A(c|g|l|m|r|s|t|u)|"
-    "B(a|e|h|i|k|r)?|"
-    "C(a|d|e|f|l|m|n|o|r|s|u)?|"
-    "D(b|s|y)|"
-    "E(r|s|u)|"
-    "F(e|l|m|r)?|"
-    "G(a|d|e)|"
-    "H(e|f|g|o|s)?|"
-    "I(n|r)?|"
-    "Kr?|"
-    "L(a|i|r|u|v)?|"
-    "M(c|g|n|o|t)?|"
-    "N(a|b|d|e|h|i|o|p)?|"
-    "O(g|s)?|"
-    "P(a|b|d|m|o|r|t|u)?|"
-    "R(a|b|e|f|g|h|n|u)|"
-    "S(b|c|e|g|i|m|n|r)?|"
-    "T(a|b|c|e|h|i|l|m|s)|"
-    "U|V|W|Xe|Yb?|Z(n|r)|"
-    "b|c|n|o|p|se?|as"
-)
-Chiral = Lit("@") + Opt(Lit("@"))
-Digit = Regex("[0-9]")
-Fifteen = (Lit("1") + Opt(oneOf("012345"))) ^ oneOf("23456789")
-Charge = (Lit("+") + Opt(Lit("+") ^ Fifteen)) ^ (Lit("-") + Opt(Lit("-") ^ Fifteen))
-HCount = Lit("H") + Opt(Digit)
-Isotope = Opt(Digit) + Opt(Digit) + Digit
-Map = Lit(":") + Opt(Digit) + Opt(Digit) + Digit
-Dot = Lit(".")
-Bond = oneOf("-=#$/\\")
-Line = pp.Forward()
-Atom = pp.Forward()
-RNum = Digit ^ (Lit("%") + Digit + Digit)
-Branch = Lit("(") + (pp.OneOrMore(Opt(Bond ^ Dot) + Line)) + Lit(")")
-Chain = pp.OneOrMore((Dot + Atom) ^ (Opt(Bond) + (Atom ^ RNum)))
-BracketAtom = Lit("[") + Opt(Isotope) + Symbol + Opt(Chiral) + Opt(HCount) + Opt(Charge) ^ Opt(Map) + Lit("]")
-Atom = OrganicSymbol ^ BracketAtom
-Line = Atom + pp.ZeroOrMore(Chain ^ Branch)
+pp.ParserElement.enablePackrat()
 
 
 class SMILESParser:
-    @classmethod
-    def is_valid(cls, text):
-        results = Line.parseString(text)
-        return True if results else False
+    def __init__(self):
+        OrganicSymbol = Regex("Br?|Cl?|N|O|P|S|F|I|At|Ts|b|c|n|o|p|s").setParseAction(self.parse)
+        Symbol = Regex(
+            "A(c|g|l|m|r|s|t|u)|"
+            "B(a|e|h|i|k|r)?|"
+            "C(a|d|e|f|l|m|n|o|r|s|u)?|"
+            "D(b|s|y)|"
+            "E(r|s|u)|"
+            "F(e|l|m|r)?|"
+            "G(a|d|e)|"
+            "H(e|f|g|o|s)?|"
+            "I(n|r)?|"
+            "Kr?|"
+            "L(a|i|r|u|v)?|"
+            "M(c|g|n|o|t)?|"
+            "N(a|b|d|e|h|i|o|p)?|"
+            "O(g|s)?|"
+            "P(a|b|d|m|o|r|t|u)?|"
+            "R(a|b|e|f|g|h|n|u)|"
+            "S(b|c|e|g|i|m|n|r)?|"
+            "T(a|b|c|e|h|i|l|m|s)|"
+            "U|V|W|Xe|Yb?|Z(n|r)|"
+            "b|c|n|o|p|se?|as"
+        )
+        Chiral = Regex("@@?")
+        Fifteen = Regex("1(0|1|2|3|4|5)|2|3|4|5|6|7|8|9")
+        Charge = ((Lit("+") + Opt(Lit("+") ^ Fifteen)) ^ (Lit("-") + Opt(Lit("-") ^ Fifteen))).setParseAction(lambda x: int(x[0]))
+        HCount = Regex("H[0-9]?")
+        Isotope = Regex("[0-9]?[0-9]?[0-9]")
+        Map = Regex(":[0-9]?[0-9]?[0-9]")
+        Dot = Lit(".").setParseAction(self._parse_dot)
+        Bond = Regex(r"-|=|#|$|\\").setParseAction(self._parse_bond)
+        RNum = Regex("[0-9]|(%[0-9][0-9])").setParseAction(self._parse_ring_num)
+        Line = pp.Forward()
+        Atom = pp.Forward()
+        LBr = Lit("(").setParseAction(self._parse_lbr)
+        RBr = Lit(")").setParseAction(self._parse_rbr)
+        Branch = (LBr + (pp.OneOrMore(Opt(Bond ^ Dot) + Line)) + RBr)
+        Chain = pp.OneOrMore((Dot + Atom("atoms")) ^ (Opt(Bond("explicit_bonds")) + (Atom("atoms") ^ RNum("rings"))))
+        BracketAtom = Lit("[") + Opt(Isotope("isotopes")) + Symbol + Opt(Chiral("chirality")) + Opt(HCount("explicit_hydrogens")) + Opt(Charge) ^ Opt(Map) + Lit("]")
+        Atom << (OrganicSymbol ^ BracketAtom).setParseAction(self._parse_atom)
+        Line << Atom("atom") + pp.ZeroOrMore(Chain ^ Branch)
+        self.parser = Line
 
-    @classmethod
-    def parse(cls, text):
-        return Line.parseString(text)
+    def parse(self, tok):
+        print(tok, self.count)
+        self.count += 1
+
+    def _parse_atom(self, tok):
+        N = len(self.atoms)
+        if self._prev_idx > -1:
+            self.bonds.append((self._prev_idx, N + 1, self._bond_type))
+            print("adding bond", self.bonds[-1])
+        self._bond_type = "-"
+        self.atoms.append(tok[0])
+        self._prev_idx = N + 1
+
+    def _parse_dot(self, tok):
+        self._prev_idx = -1
+
+    def _parse_bond(self, tok):
+        self._bond_type = tok[0]
+
+    def _parse_ring_num(self, toks):
+        idx = int(toks[0])
+        print("parse ring num")
+        if idx in self._rings:
+            self._rings[idx] = (self._rings[idx][0], len(self.atoms))
+        else:
+            self._rings[idx] = (len(self.atoms), -1)
+
+    def _parse_lbr(self, toks):
+        self._branch_idx = self._prev_idx
+        print("lbr", self._prev_idx)
+
+    def _parse_rbr(self, toks):
+        self._prev_idx = self._branch_idx
+        self._branch_idx = None
+        print("rbr", self._prev_idx)
+
+    def parseString(self, s):
+        self.count = 0
+        self._prev_idx = -1
+        self._branch_idx = -1
+        self.atoms = []
+        self.bonds = []
+        self._bond_type = "-"
+        self._rings = {}
+        self.parser.parseString(s)
+        print(self.atoms)
+        print(self.bonds)
+        print(self._rings)
+
 
 def parse(s):
-    return SMILESParser.parse(s)
+    return SMILESParser().parseString(s)
