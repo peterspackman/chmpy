62a031fe62075b7c3b4ba3b3cd80bdfd7b3d6e0d
Peter Spackman <peterspackman@fastmail.com>
2020-05-22 13:59:15 +0800

    Added basic smiles parser test

---
+++ tests/fmt/test_smiles.py
@@ -0,0 +1,23 @@
+import unittest
+import numpy as np
+from tempfile import TemporaryDirectory
+from pathlib import Path
+import logging
+from .. import TEST_FILES
+from chmpy.fmt.smiles import parse
+
+LOG = logging.getLogger(__name__)
+_WATER = "O"
+_CUBANE = "C12C3C4C1C5C4C3C25"
+
+class SMILESParserTest(unittest.TestCase):
+    def test_parse_valid(self):
+        from pyparsing import ParseException
+        with self.assertRaises(ParseException):
+            parse("invalid")
+        parse(_WATER)
+
+    def test_cubane_bonds(self):
+        atoms, bonds = parse(_CUBANE)
+        self.assertEqual(len(bonds), 12)
+
