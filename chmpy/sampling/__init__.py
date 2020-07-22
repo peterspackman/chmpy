0f375a32357bb7abf7805f5f2d369661897b8e33
Peter Spackman <peterspackman@fastmail.com>
2020-07-22 11:39:34 +0800

    Fixed sampling __init__.py

--- chmpy/sampling/__init__.py
+++ chmpy/sampling/__init__.py
@@ -3,7 +3,7 @@ from ._sobol import quasirandom_sobol, quasirandom_sobol_batch
 
 __all__ = [
     "quasirandom_kgf",
-    "quasirandom_kgf_batch"
+    "quasirandom_kgf_batch",
     "quasirandom_sobol",
-    "quasirandom_sobol_batch"
+    "quasirandom_sobol_batch",
 ]
