Fix issue with SHT sign

chmpy/shape/reconstruct.py
@@ -1,8 +1,6 @@
 import logging
 import numpy as np
 from .sht import SHT
-from trimesh import PointCloud
-from chmpy.util.num import cartesian_to_spherical
 
 LOG = logging.getLogger(__name__)
 
@@ -11,27 +9,28 @@ def reconstruct(coefficients, real=True):
     if real:
         n = len(coefficients)
         l_max = int((-3 + np.sqrt(8 * n + 1)) // 2)
-        func = "synth_real"
     else:
         l_max = int(np.sqrt(len(coefficients))) - 1
-        func = "synth_cplx"
     LOG.debug("Reconstructing deduced l_max = %d", l_max)
     sht = SHT(l_max)
     x, y, z = sht.grid_cartesian
     pts = np.c_[x.flatten(), y.flatten(), z.flatten()]
     pts = pts * sht.synthesis(coefficients).flatten()[:, np.newaxis].real
-    g = np.empty((sht.grid[0].size, 2), dtype=np.float32)
-    g[:, 0] = sht.grid[0].flatten()
-    g[:, 1] = sht.grid[1].flatten()
-    return np.c_[pts, g]
+    return pts
 
 
-def reconstructed_surface(coefficients, real=True, subdivisions=3):
-    raise NotImplementedError("Not implmented yet for new SHT yet")
+def reconstructed_surface_convex(coefficients, real=True):
+    from trimesh import Trimesh
+    from scipy.spatial import ConvexHull
+    pts = reconstruct(coefficients, real=real)
+    cvx = ConvexHull(pts)
+    return Trimesh(vertices=pts, faces=cvx.simplices)
+
+
+def reconstructed_surface_icosphere(coefficients, real=True, subdivisions=3):
     if real:
         n = len(coefficients)
         l_max = int((-3 + np.sqrt(8 * n + 1)) // 2)
-        func = "synth_real"
     else:
         raise NotImplementedError(
             "Complex reconstructed surface case not yet implemented"
@@ -42,10 +41,10 @@ def reconstructed_surface(coefficients, real=True, subdivisions=3):
     from trimesh.creation import icosphere
 
     sphere = icosphere(subdivisions=subdivisions)
-    rtp = cartesian_to_spherical(sphere.vertices)
-    for i in range(rtp.shape[0]):
-        cost = np.cos(rtp[i, 2])
-        phi = rtp[i, 1]
-        rtp[i, 0] = sht._shtns.SH_to_point(coefficients, cost, phi)
-    sphere.vertices *= rtp[:, 0, np.newaxis]
+    theta = np.arccos(sphere.vertices[:, 2])
+    phi = np.arctan2(sphere.vertices[:, 1], sphere.vertices[:, 0])
+    r = np.empty_like(phi)
+    for i in range(phi.shape[0]):
+        r[i] = sht.evaluate_at_points(coefficients, theta[i], phi[i])
+    sphere.vertices *= r[:, np.newaxis]
     return sphere

chmpy/shape/sht.py
@@ -11,7 +11,7 @@ from scipy.fft import fft, ifft
 _SHT_CACHE = {}
 
 class SHT:
-    def __init__(self, lm, nphi=None):
+    def __init__(self, lm, nphi=None, ntheta=None):
         self.lmax = lm
         self.plm = AssocLegendre(lm)
 
@@ -21,9 +21,14 @@ class SHT:
             self.nphi = nphi
         # avoid the poles
         self.phi = np.arange(0, self.nphi) * 2 * np.pi / self.nphi
-        self.ntheta = 1
-        while (self.ntheta <= self.nphi):
-            self.ntheta *= 2
+
+        if ntheta is None:
+            n = self.lmax + 1
+            n += (n & 1)
+            n = ((n + 3) // 4) * 4
+            self.ntheta = n
+        else:
+            self.ntheta = ntheta
         
         self.cos_theta, self.weights, self.total_weight = roots_legendre(self.ntheta, mu=True)
         self.weights *= 4 * np.pi / self.total_weight