53ffa4ed6a5b85605a8e4c87c96c7b7e90ba8607
Peter Spackman <peterspackman@fastmail.com>
2025-05-21 09:42:45 +0800

    Add spherical harmonics implementation

---
+++ src/chmpy/shape/spherical_harmonics.py
@@ -0,0 +1,108 @@
+import numpy as np
+from ._sht import AssocLegendre
+
+
+class SphericalHarmonics:
+    def __init__(self, lm, phase=True):
+        self.lmax = lm
+        self.phase = phase
+        self.plm = AssocLegendre(lm)
+        self.plm_work_array = np.empty(self.nplm(), dtype=np.float64)
+
+    def idx_c(self, l, m):
+        return l * (l + 1) + m
+
+    def nlm(self):
+        "the number of complex SHT coefficients"
+        return (self.lmax + 1) * (self.lmax + 1)
+
+    def nplm(self):
+        "the number of real SHT coefficients (i.e. legendre polynomial terms)"
+        return (self.lmax + 1) * (self.lmax + 2) // 2
+
+    def single_angular(self, theta, phi, result=None):
+        if result is None:
+            result = np.empty(self.nlm(), dtype=np.complex128)
+
+        ct = np.cos(theta)
+        self.plm.evaluate_batch(ct, result=self.plm_work_array)
+
+        plm_idx = 0
+        for l in range(0, self.lmax + 1):
+            offset = l * (l + 1)
+            result[offset] = self.plm_work_array[plm_idx]
+            plm_idx += 1
+
+        c = np.exp(phi * 1j)
+        cm = c
+        for m in range(1, self.lmax + 1):
+            sign = 1
+            if self.phase and (m & 1):
+                sign = -1
+            for l in range(m, self.lmax + 1):
+                l_offset = l * (l + 1)
+                rr = cm
+                ii = np.conj(rr)
+                rr = sign * self.plm_work_array[plm_idx] * rr
+                ii = sign * self.plm_work_array[plm_idx] * ii
+                if m & 1:
+                    ii = -ii
+                result[l_offset - m] = ii
+                result[l_offset + m] = rr
+                plm_idx += 1
+            cm *= c
+        return result
+
+    def batch_angular(self, pos, result=None):
+        if result is None:
+            result = np.empty((pos.shape[0], self.nlm()), dtype=np.complex128)
+
+        for i in range(pos.shape[0]):
+            result[i, :] = self.single_angular(*pos[i], result=result[i, :])
+        return result
+
+    def single_cartesian(self, x, y, z, result=None):
+        if result is None:
+            result = np.empty(self.nlm(), dtype=np.complex128)
+        pass
+        epsilon = 1e-12
+        ct = z
+        self.plm.evaluate_batch(ct, result=self.plm_work_array)
+
+        st = 0.0
+        if abs(1.0 - ct) > epsilon:
+            st = np.sqrt(1.0 - ct * ct)
+
+        plm_idx = 0
+        for l in range(0, self.lmax + 1):
+            l_offset = l * (l + 1)
+            result[l_offset] = self.plm_work_array[plm_idx]
+            plm_idx += 1
+
+        c = 0j
+        if st > epsilon:
+            c = complex(x / st, y / st)
+        cm = c
+        for m in range(1, self.lmax + 1):
+            sign = 1
+            if self.phase and (m & 1):
+                sign = -1
+            for l in range(m, self.lmax + 1):
+                l_offset = l * (l + 1)
+                rr = sign * cm * self.plm_work_array[plm_idx]
+                ii = sign * np.conj(cm) * self.plm_work_array[plm_idx]
+                if m & 1:
+                    ii = -ii
+                result[l_offset - m] = ii
+                result[l_offset + m] = rr
+                plm_idx += 1
+            cm *= c
+        return result
+
+    def batch_cartesian(self, pos, result=None):
+        if result is None:
+            result = np.empty((pos.shape[0], self.nlm()), dtype=np.complex128)
+        pass
+
+    def __eval__(self, *parameters, result=None, cartesian=False):
+        pass
---
+++ src/chmpy/tests/shape/test_spherical_harmonics.py
@@ -0,0 +1,51 @@
+import unittest
+import math
+from chmpy.shape.spherical_harmonics import SphericalHarmonics
+
+
+class TestSphericalHarmonics(unittest.TestCase):
+    def setUp(self):
+        self.sph = SphericalHarmonics(4, True)
+
+    def cmp(self, a, b, delta=1e-12):
+        self.assertAlmostEqual(a.real, b.real, delta=delta)
+        self.assertAlmostEqual(a.imag, b.imag, delta=delta)
+
+    def test_angular(self):
+        eval_result = self.sph.single_angular(math.pi / 4, math.pi / 4)
+        self.cmp(eval_result[0], complex(0.28209479177387814, 0.0))
+        self.cmp(eval_result[1], complex(0.17274707473566775, -0.17274707473566772))
+        self.cmp(eval_result[2], complex(0.34549414947133544, 0))
+        self.cmp(eval_result[3], complex(-0.17274707473566775, -0.17274707473566772))
+        self.cmp(eval_result[4], complex(1.1826236627522426e-17, -0.19313710101159473))
+        self.cmp(eval_result[5], complex(0.27313710764801974, -0.2731371076480197))
+        self.cmp(eval_result[6], complex(0.15769578262626002, 0))
+        self.cmp(eval_result[7], complex(-0.2731371076480198, -0.27313710764801974))
+        self.cmp(eval_result[8], complex(1.1826236627522428e-17, 0.19313710101159476))
+        self.cmp(eval_result[9], complex(-0.104305955908196, -0.10430595590819601))
+        self.cmp(eval_result[10], complex(2.212486281755292e-17, -0.3613264303300692))
+        self.cmp(eval_result[11], complex(0.24238513808561293, -0.24238513808561288))
+        self.cmp(eval_result[12], complex(-0.13193775767639848, 0))
+        self.cmp(eval_result[13], complex(-0.24238513808561296, -0.24238513808561293))
+        self.cmp(eval_result[14], complex(2.2124862817552912e-17, 0.3613264303300691))
+        self.cmp(eval_result[15], complex(0.104305955908196, -0.10430595590819601))
+        self.cmp(
+            eval_result[16], complex(-0.11063317311124561, -1.3548656133020197e-17)
+        )
+        self.cmp(eval_result[17], complex(-0.22126634622249125, -0.2212663462224913))
+        self.cmp(eval_result[18], complex(2.5604553376501068e-17, -0.4181540897233056))
+        self.cmp(eval_result[19], complex(0.08363081794466115, -0.08363081794466114))
+        self.cmp(eval_result[20], complex(-0.34380302747441394, 0))
+        self.cmp(eval_result[21], complex(-0.08363081794466115, -0.08363081794466114))
+        self.cmp(eval_result[22], complex(2.5604553376501068e-17, 0.4181540897233056))
+        self.cmp(eval_result[23], complex(0.22126634622249125, -0.2212663462224913))
+        self.cmp(eval_result[24], complex(-0.11063317311124561, 1.3548656133020197e-17))
+
+    def test_cartesian(self):
+        pos = [2.0, 0.0, 1.0]
+        theta = 0.0
+        phi = 0.0
+        eval_result = self.sph.single_cartesian(*pos)
+        eval_ang = self.sph.single_angular(theta, phi)
+        for i in range(25):
+            self.cmp(eval_result[i], eval_ang[i])
